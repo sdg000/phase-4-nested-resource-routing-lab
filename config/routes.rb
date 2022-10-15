a081410516fdd698d0f144bea3884417f6816b2f
Seth David Gyimah <esec26@gmail.com>
2022-10-15 09:45:35 +0000

    completed lab requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index, :create]
+
+  # nested routes showing a user and it's related items
+  resources :users, only: [:show] do 
+    resources :items, only: [:show, :index, :create]
+  end
 end
